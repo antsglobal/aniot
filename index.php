021de8eed65b78eb275c80ca984215f3e92f58de
antsglobal <60220453+antsglobal@users.noreply.github.com>
2020-02-18 15:03:13 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
-<h1 style="text-align: center;">ALPHA-NUMERO IOT DASHBOARD</h1>
+<h1 style="text-align: center;"><span style="background-color: #ff6600;">ALPHA-NUMERO IOT DASHBOARD</span></h1>
 <p style="text-align: center;"><span style="text-align: center; color: #0000ff;">WEATHER MONITORING SYSTEM&nbsp;</span></p>
 <table style="height: 55px; width: 595px; margin-left: auto; margin-right: auto;">
 <tbody>
 <tr>
-<td style="width: 283px; text-align: center;">&nbsp;TEMPERATURE</td>
-<td style="width: 296px; text-align: center;">TIME STAMP</td>
+<td style="width: 283px; text-align: center;"><strong>&nbsp;TEMPERATURE</strong></td>
+<td style="width: 296px; text-align: center;"><strong>TIME STAMP</strong></td>
 </tr>
 </tbody>
 </table>
