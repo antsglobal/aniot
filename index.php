9bd5311668fb2583df3bd448a3a6377ec977c003
antsglobal <60220453+antsglobal@users.noreply.github.com>
2020-02-18 14:54:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<h1> ALPHA NUMERO  </h1>
-
-<?php
-echo "IOT WEBPAGE";
-?>
-
-</body>
-</html>
+<p>ALPHA-NUMERO IOT DASHBOARD</p>
+<table style="height: 39px; width: 222px;">
+<tbody>
+<tr>
+<td style="width: 103px;">&nbsp;TEMPERATURE</td>
+<td style="width: 113px;">TIMESTAMP</td>
+</tr>
+</tbody>
+</table>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
