2eadbc5bc2848d9458eb476871bc5177271d5280
antsglobal <60220453+antsglobal@users.noreply.github.com>
2020-02-18 15:08:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,30 @@
 <h1 style="text-align: center;"><span style="background-color: #ff6600;">ALPHA-NUMERO IOT DASHBOARD</span></h1>
 <p style="text-align: center;"><span style="text-align: center; color: #0000ff;">WEATHER MONITORING SYSTEM&nbsp;</span></p>
-<table style="height: 55px; width: 595px; margin-left: auto; margin-right: auto;">
+<table style="height: 56px; width: 596px; margin-left: auto; margin-right: auto;">
 <tbody>
 <tr>
 <td style="width: 283px; text-align: center;"><strong>&nbsp;TEMPERATURE</strong></td>
-<td style="width: 296px; text-align: center;"><strong>TIME STAMP</strong></td>
+<td style="width: 297px; text-align: center;"><strong>TIME STAMP</strong></td>
+</tr>
+<tr>
+<td style="width: 283px; text-align: center;">23.23</td>
+<td style="width: 297px; text-align: center;">&nbsp;</td>
+</tr>
+<tr>
+<td style="width: 283px; text-align: center;">24.13</td>
+<td style="width: 297px; text-align: center;">&nbsp;</td>
+</tr>
+<tr>
+<td style="width: 283px; text-align: center;">25.87</td>
+<td style="width: 297px; text-align: center;">&nbsp;</td>
+</tr>
+<tr>
+<td style="width: 283px; text-align: center;">24.90</td>
+<td style="width: 297px; text-align: center;">&nbsp;</td>
+</tr>
+<tr>
+<td style="width: 283px; text-align: center;">24.23</td>
+<td style="width: 297px; text-align: center;">&nbsp;</td>
 </tr>
 </tbody>
 </table>
-<p style="text-align: center;">&nbsp;</p>
-<p style="text-align: center;">&nbsp;</p>
