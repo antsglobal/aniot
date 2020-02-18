68576c458662ebd07d082026b0e17e72919d183a
antsglobal <60220453+antsglobal@users.noreply.github.com>
2020-02-18 16:05:57 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <html>
 <head> 
-<title>Kernel Masters</title>
+<title>ALPHA NUMERO</title>
 <!--<meta http-equiv="refresh" content="3">--!>
 </head>
 <body align="center">
 <table  align="center">
 <tr><td><img height="80px" src="logo.png" align="center"></img></td></tr>
-<tr><td align="center"><br/><font size="6" color="#0099CC" face="calibri"><b>Smart Attendance System</b></font></td></tr>
+<tr><td align="center"><br/><font size="6" color="#0099CC" face="calibri"><b>IOT DASHBOARD</b></font></td></tr>
 </table>
 </br>
 
