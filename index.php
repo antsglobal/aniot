cc983efc0d2c17edd577061b115070a27f89d9f1
antsglobal <60220453+antsglobal@users.noreply.github.com>
2020-02-18 14:30:38 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,12 @@
+<!DOCTYPE html>
 <html>
-<head> 
-<title>Kernel Masters</title>
-<!--<meta http-equiv="refresh" content="3">--!>
-</head>
-<body align="center">
-<table  align="center">
-<tr><td><img height="80px" src="logo.png" align="center"></img></td></tr>
-<tr><td align="center"><br/><font size="6" color="#0099CC" face="calibri"><b>Smart Attendance System</b></font></td></tr>
-</table>
-</br>
+<body>
 
-<center>
+<h1>My first PHP page</h1>
 
-<iframe frameborder="0" height="600"  width="800" marginheight="10" marginwidth="0" id="dynamic-content" src="displayattendancedata.php" scrolling="yes"></iframe>
+<?php
+echo "Hello World!";
+?>
 
- <br><br>
-
-<!--<button onclick="window.location='excel-download.php'">Click here to download this data in excel file</button>
---!>
-
-</center>
 </body>
 </html>
