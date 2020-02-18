1cd88208739df645683c61750c4ba206b7c54d27
antsglobal <60220453+antsglobal@users.noreply.github.com>
2020-02-18 15:11:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,30 +1,34 @@
 <h1 style="text-align: center;"><span style="background-color: #ff6600;">ALPHA-NUMERO IOT DASHBOARD</span></h1>
-<p style="text-align: center;"><span style="text-align: center; color: #0000ff;">WEATHER MONITORING SYSTEM&nbsp;</span></p>
+<h2 style="text-align: center;"><strong><span style="text-decoration: underline;"><span style="text-align: center; color: #0000ff; text-decoration: underline;">WEATHER MONITORING SYSTEM&nbsp;</span></span></strong></h2>
 <table style="height: 56px; width: 596px; margin-left: auto; margin-right: auto;">
 <tbody>
 <tr>
-<td style="width: 283px; text-align: center;"><strong>&nbsp;TEMPERATURE</strong></td>
-<td style="width: 297px; text-align: center;"><strong>TIME STAMP</strong></td>
+<td style="width: 283px; text-align: center;">
+<h3><strong>&nbsp;TEMPERATURE</strong></h3>
+</td>
+<td style="width: 297px; text-align: center;">
+<h3><strong>TIME STAMP</strong></h3>
+</td>
 </tr>
 <tr>
 <td style="width: 283px; text-align: center;">23.23</td>
-<td style="width: 297px; text-align: center;">&nbsp;</td>
+<td style="width: 297px; text-align: center;">15:07:14</td>
 </tr>
 <tr>
 <td style="width: 283px; text-align: center;">24.13</td>
-<td style="width: 297px; text-align: center;">&nbsp;</td>
+<td style="width: 297px; text-align: center;">15:08:17</td>
 </tr>
 <tr>
 <td style="width: 283px; text-align: center;">25.87</td>
-<td style="width: 297px; text-align: center;">&nbsp;</td>
+<td style="width: 297px; text-align: center;">15:09:12</td>
 </tr>
 <tr>
 <td style="width: 283px; text-align: center;">24.90</td>
-<td style="width: 297px; text-align: center;">&nbsp;</td>
+<td style="width: 297px; text-align: center;">15:10:18</td>
 </tr>
 <tr>
 <td style="width: 283px; text-align: center;">24.23</td>
-<td style="width: 297px; text-align: center;">&nbsp;</td>
+<td style="width: 297px; text-align: center;">15:11:12</td>
 </tr>
 </tbody>
 </table>
