a653c4f8c1a632a05ceb845fa93bcdb69e5a3bd6
antsglobal <60220453+antsglobal@users.noreply.github.com>
2020-02-19 13:29:41 +0530

    Update data.php

--- data.php
+++ data.php
@@ -1,17 +1,20 @@
 <?php
-
-//getting the data from the data base
-$conn = mysqli_connect("numero.database.windows.net", "numero", "Alpha@123$", "ANIOT_TEMP");
-
-//getting the data from the employee table
-$result = mysqli_query($conn, "SELECT Temperature FROM ANIOT_TEMP");
-
-//storing it the array 
-$data = array();
-while ($row = mysqli_fetch_object($result))
-{
-    array_push($data, $row);
-}
-
-echo json_encode($data);
-exit();
+    $serverName = "numero.database.windows.net"; // update me
+    $connectionOptions = array(
+        "Database" => "numero", // update me
+        "Uid" => "numero", // update me
+        "PWD" => "Alpha@123$" // update me
+    );
+    //Establishes the connection
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    echo("Connection successfull");
+    $tsql= "SELECT * FROM ANIOT_TEMP";
+    $getResults= sqlsrv_query($conn, $tsql);
+    echo ("Reading data from table" . PHP_EOL);
+    if ($getResults == FALSE)
+        echo (sqlsrv_errors());
+    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+     echo ($row['CategoryName'] . " " . $row['ProductName'] . PHP_EOL);
+    }
+    sqlsrv_free_stmt($getResults);
+?>
